a8ce5ea33556dba820c569c86c6ac05aba665f29
Pedro augusto <Pedroaugusto.borges36@gmail.com>
2025-04-05 08:07:28 -0300

    Criando classes e aplicando o código

--- SistemaSmartTv/src/NewPackage/SmartTv.java
+++ SistemaSmartTv/src/NewPackage/SmartTv.java
@@ -1,5 +1,51 @@
 package NewPackage;
 
 public class SmartTv {
-
+	
+			boolean ligada=false;
+			int canal=1;
+			int volume=25;
+			
+			public void ligar() 
+			{
+				
+				ligada=true;
+			}
+			
+			
+			
+			public void desligar() 
+			{
+				
+				ligada=false;
+			}
+			
+			public void canal() 
+			{
+				
+				canal=++canal;
+				volume=25;
+			}
+			
+			public void aumentarVolume() 
+			{
+				
+				volume++;
+			}
+			
+			public void diminuirVolume() 
+			{
+				
+				volume--;
+			}
+			
+			
+			
 }
+
+			
+				
+				
+
+
+
---
+++ SistemaSmartTv/src/NewPackage/Usuario.java
@@ -0,0 +1,27 @@
+package NewPackage;
+
+public class Usuario {
+	
+		public static void main(String[] args) throws Exception {
+			
+			SmartTv  smartTv=new SmartTv();
+			
+			System.out.println("Tv ligada ? " + smartTv.ligada);
+			System.out.println("Canal Atual ? "+ smartTv.canal);
+			System.out.println("Volume Atual ? "+ smartTv.volume);
+			
+			smartTv.ligar();
+			System.out.println("Novo status --> Tv ligada ?: " + smartTv.ligada);
+			
+			smartTv.desligar();
+			System.out.println("Novo status --> Tv ligada ?: " + smartTv.ligada);
+			
+			
+			smartTv.canal();
+			System.out.println("Novo status --> Canal selecionado ?: " + smartTv.canal+"\n Volume atual: "+ smartTv.volume+"");
+			
+			
+			
+		}
+
+}
